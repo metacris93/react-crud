a4316903145b181d7d3900e51d3ac407ff38a420
Cristian Pisco <metacris93@gmail.com>
2021-05-24 01:15:19 -0500

    agregando componente Loader que se muestra cuando realiza una peticion HTTP

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class BadgesList extends React.Component {
     render () {
+        if (this.props.badges.length === 0) {
+            return (
+                <div>
+                    <h3>Badges not found</h3>
+                </div>
+            );
+        }
         return (
             <ul className="list-unstyled">
                 { this.props.badges.map((badge) => {
